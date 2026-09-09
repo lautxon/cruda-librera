8383abc8cf0405991423183b6b2af82c6e523586
lautxon <125925421+lautxon@users.noreply.github.com>
2026-09-09 01:12:20 -0300

    Implement dynamic rendering of blog posts
    Add functionality to render blog posts dynamically.

--- blog.js
+++ blog.js
@@ -1,1 +1,11 @@
-
+const blogPosts = document.getElementById("blog-posts");
+if (blogPosts) {
+  blogPosts.innerHTML = BLOG_POSTS.map(post => `
+    <article class="blog-post">
+      <p class="blog-post-date">${post.date}</p>
+      <h3>${post.title}</h3>
+      <p class="blog-post-excerpt">${post.excerpt}</p>
+      <p class="blog-post-author">${post.author}</p>
+    </article>
+  `).join("");
+}
